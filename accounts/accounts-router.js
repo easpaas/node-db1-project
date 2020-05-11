4a26334353afdbacb6ae5fbe6b8adc463f8a710b
evan <aspaasevan@gmail.com>
2020-05-11 14:02:56 -0700

    PUT request successful

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -17,36 +17,63 @@ router.get("/", (req, res) => {
     });
 });
 
-// router.get("/:id", (req, res) => {
-//   db("accounts")
-//     .where({ id: req.params.id })
-//     .first()
-//     .then(count => {
-//       console.log(count);
-//     })
-//     .catch(error => {
-//       console.log(error);
-//       res.status(500).json({ errorMessage: error.message });
-//     });
-// });
+router.get("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .first()
+    .then(count => {
+      console.log(count);
+      res.status(200).json({ data: count });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: error.message });
+    });
+});
 
 router.post("/", (req, res) => {
   const post = req.body;
 
-  isValidPost(post) ?
-    db("accounts")
-      .insert(post, "id")
-      .then(ids => {
-        console.log(ids)
-        res.status(201).json({ data: ids })
-      })
-      .catch(error => {
-        console.log(error)
-        res.status(500).json({ errorMessage: error.message });
-      })
-  :
-    res.status(400).json({ errorMessage: 'Please provide both name and budget to create an account.'})
+  isValidPost(post)
+    ? db("accounts")
+        .insert(post, "id")
+        .then(ids => {
+          console.log(ids);
+          res.status(201).json({ data: ids });
+        })
+        .catch(error => {
+          console.log(error);
+          res.status(500).json({ errorMessage: error.message });
+        })
+    : res
+        .status(400)
+        .json({
+          errorMessage:
+            "Please provide both name and budget to create an account."
+        });
+});
+
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
+
+  db("accounts")
+    .where({ id })
+    .update(changes)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ data: count });
+      } else {
+        res
+          .status(404)
+          .json({ errorMessage: "Could not find any post by that id" });
+      }
     })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: error.message });
+    });
+});
 
 function isValidPost(post) {
   return Boolean(post.name && post.budget);
